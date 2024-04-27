f6ce2bb2e4b889ce3e041de8ef6c240a37e103a6
Gio Perrotta <giovanni.perrotta@code7.com>
2024-04-27 15:12:04 -0300

    created home page structure

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import styles from './home.module.scss'
+
+const HomePage = () => (
+  <div className={styles.container}>
+    <header>
+      <div className={styles.header_content}>header valorant</div>
+    </header>
+    <main>main</main>
+  </div>
+)
+
+export default HomePage
