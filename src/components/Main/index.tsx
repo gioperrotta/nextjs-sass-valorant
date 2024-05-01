724d7eab206b0ddc7626e886a8ccb1755544d139
Gio Perrotta <giovanni.perrotta@code7.com>
2024-05-01 11:28:37 -0300

    feat(card-0000): added carrousel with Swiper Lib

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,12 @@
+'use client'
 import Image from 'next/image'
 import styles from './style.module.scss'
 
 import { agent } from '@/data/agentes'
 import { Agent } from '../Agent'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
 
 export const Main = () => (
   <main className={styles.container}>
@@ -17,13 +21,34 @@ export const Main = () => (
       <h1>AGENTES</h1>
 
     </div>
-    <div className={styles.agents}>
-      <Agent agent={agent}/>
-      <Agent agent={agent}/>
-      <Agent agent={agent}/>
-      <Agent agent={agent}/>
-
-
-    </div>
+    <Swiper className={styles.agents} spaceBetween={16} slidesPerView={4}>
+      <SwiperSlide>
+        <Agent agent={agent} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Agent agent={agent} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Agent agent={agent} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Agent agent={agent} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Agent agent={agent} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Agent agent={agent} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Agent agent={agent} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Agent agent={agent} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Agent agent={agent} />
+      </SwiperSlide>
+    </Swiper>
   </main>
 )
